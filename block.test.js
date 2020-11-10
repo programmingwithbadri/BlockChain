66db6cbe5e585a3c8b3487f5fb4f4c8d2d739283
Badri Narayanan <programmingwithbadri@outlook.com>
2020-11-10 22:39:32 +0530

    Mine Block(latest block/ or newly created block in the block-chain) - TDD

--- block.test.js
+++ block.test.js
@@ -32,4 +32,27 @@ describe('Genesis Block', () => {
   it('returns the Genesis data', () => {
     expect(genesisBlock).toEqual(GENESIS_DATA);
   });
+});
+
+describe('Mine Block', () => {
+  const lastBlock = Block.genesis();
+  const data = 'mined data';
+
+  const minedBlock = Block.mineBlock({ lastBlock, data });
+
+  it('returns a Block instance', () => {
+    expect(minedBlock instanceof Block).toBe(true);
+  });
+
+  it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+    expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+  });
+
+  it('sets the `data`', () => {
+    expect(minedBlock.data).toEqual(data);
+  });
+
+  it('sets the `timestamp`', () => {
+    expect(minedBlock.timestamp).not.toEqual(undefined);
+  });
 })
