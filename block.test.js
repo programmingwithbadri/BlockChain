9e6f286f51bf3c6a292a96a4035bfe79380f3d44
Badri Narayanan <programmingwithbadri@outlook.com>
2020-12-16 22:30:00 +0530

    Difficulty and the Nonce Value - Tests

--- block.test.js
+++ block.test.js
@@ -7,12 +7,16 @@ describe("Block", () => {
   const lastHash = 'last-hash';
   const data = 'TDD data'
   const hash = 'current-block-hash';
+  const nonce = 1;
+  const difficulty = 1;
 
   const block = new Block({
     timestamp,
     lastHash,
     data,
     hash,
+    nonce,
+    difficulty
   });
 
   it('should have timestamp, data, lastHash and hash property', () => {
@@ -20,6 +24,8 @@ describe("Block", () => {
     expect(block.data).toEqual(data);
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
+    expect(block.nonce).toEqual(nonce);
+    expect(block.difficulty).toEqual(difficulty);
   });
 });
 
@@ -55,9 +61,23 @@ describe('Mine Block', () => {
 
   it('sets the `timestamp`', () => {
     expect(minedBlock.timestamp).not.toEqual(undefined);
-  }); 
+  });
 
   it('creates the SHA-256 `hash` based on proper inputs', () => {
-    expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, minedBlock.lastHash, data))
+    expect(minedBlock.hash).toEqual(
+      cryptoHash(
+        minedBlock.timestamp,
+        minedBlock.nonce,
+        minedBlock.difficulty,
+        minedBlock.lastHash,
+        data
+      )
+    )
+  });
+
+
+  it('sets a `hash` that matches the difficulty criteria', () => {
+    expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+      .toEqual('0'.repeat(minedBlock.difficulty))
   });
 })
