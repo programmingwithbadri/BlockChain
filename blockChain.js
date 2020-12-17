568edf1d130ec4c369ec73862ffceb194480b6dd
Badri Narayanan <programmingwithbadri@outlook.com>
2020-12-17 22:36:09 +0530

    Prevent difficulty juping in the block chain by validating the difficuties between blocks

--- blockChain.js
+++ blockChain.js
@@ -35,12 +35,15 @@ class BlockChain {
         for (let i = 1; i < chain.length; i++) {
             const { timestamp, hash, data, lastHash, difficulty, nonce } = chain[i];
             const actualLastHash = chain[i - 1].hash;
+            const lastDifficulty = chain[i - 1].difficulty;
 
             if (lastHash !== actualLastHash) return false;
 
             const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
             if (hash !== validatedHash) return false;
+
+            if (Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
         return true;
     }
