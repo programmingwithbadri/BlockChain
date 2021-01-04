05e32ac2b377ddc2b78a0e92787264260e2e9f18
Badri Narayanan <programmingwithbadri@outlook.com>
2021-01-04 19:22:09 +0530

    Sync chains on connect

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
 
 const DEFAULT_PORT = 3000;
 let PEER_PORT;
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+
+            blockchain.replaceChain(rootChain);
+        }
+    });
+}
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
@@ -54,5 +67,6 @@ app.post('/api/mine', (req, res) => {
 
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
-    console.log(`listening at localhost:${PORT}`);
+    console.log(`listening at localhost: ${PORT}`);
+    syncChains();
 });
