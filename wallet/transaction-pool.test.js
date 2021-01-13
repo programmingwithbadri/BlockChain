aa35581c85fc5912cfbcd1b49722eb2395852e78
Badri Narayanan <programmingwithbadri@outlook.com>
2021-01-13 20:44:52 +0530

    Create Transaction pool and set the transaction in the pool

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,26 @@
+const TransactionPool = require('./transaction-pool');
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('TransactionPool', () => {
+    let transactionPool, transaction, senderWallet;
+
+    beforeEach(() => {
+        transactionPool = new TransactionPool();
+        senderWallet = new Wallet();
+        transaction = new Transaction({
+            senderWallet,
+            recipient: 'fake-recipient',
+            amount: 50
+        });
+    });
+
+    describe('setTransaction()', () => {
+        it('adds a transaction', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(transactionPool.transactionMap[transaction.id])
+                .toBe(transaction);
+        });
+    });
+});
