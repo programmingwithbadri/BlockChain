477038d04b8bcee777d70f4aa576d04f7d2a8f1c
Badri Narayanan <programmingwithbadri@outlook.com>
2021-01-30 22:46:02 +0530

    Display transaction details in the blocks by using toggle button

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,20 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 const Block = ({ block }) => {
     const { timestamp, hash, data } = block;
+    const [shouldDisplayTransaction, setDisplayTransaction] = useState(false);
     const hashDisplay = `${hash.substring(0, 15)}...`;
 
-    const stringifiedData = JSON.stringify(data);
+    const toggleTransaction = () => {
+        setDisplayTransaction(!shouldDisplayTransaction);
+    }
 
-    const dataDisplay = stringifiedData.length > 35 ?
-        `${stringifiedData.substring(0, 35)}...` :
-        stringifiedData;
+    const displayTransaction = () => {
+        const stringifiedData = JSON.stringify(data);
+        const dataDisplay = stringifiedData.length > 35 ?
+            `${stringifiedData.substring(0, 35)}...` :
+            stringifiedData;
+
+        if (shouldDisplayTransaction) {
+            return (
+                <div>
+                    {
+                        data.map(transaction => (
+                            <div key={transaction.id}>
+                                <hr />
+                                <Transaction transaction={transaction} />
+                            </div>
+                        ))
+                    }
+                    <br />
+                    <Button
+                        bsStyle="danger"
+                        bsSize="small"
+                        onClick={toggleTransaction}
+                    >
+                        Show Less
+                    </Button>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <div>Data: {dataDisplay}</div>
+                <Button
+                    bsStyle="danger"
+                    bsSize="small"
+                    onClick={toggleTransaction}>
+                    Show More
+                </Button>
+            </div>
+        );
+    };
 
     return (
         <div className='Block'>
             <div>Hash: {hashDisplay}</div>
             <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
-            <div>Data: {dataDisplay}</div>
+            {displayTransaction()}
         </div>
     );
 }
