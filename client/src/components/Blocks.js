ef91c0ddda2898dc9f548c00598f258d0318c2fb
Badri Narayanan <programmingwithbadri@outlook.com>
2021-01-31 14:06:57 +0530

    Set pagination for the blocks frontend

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -4,18 +4,45 @@ import { Link } from 'react-router-dom';
 import Block from './Block';
 
 const Blocks = () => {
+    let paginatedId = 1;
     const [blocks, setBlocks] = useState([]);
+    const [blocksLength, setBlocksLength] = useState(0);
+
     useEffect(() => {
-        const fetchedBlocks = async () => {
-            const { data } = await axios.get(`${document.location.origin}/api/blocks`);
-            setBlocks(data);
+        const fetchedBlocksLength = async () => {
+            const { data } = await axios.get(`${document.location.origin}/api/blocks/length`);
+            setBlocksLength(data);
         }
-        fetchedBlocks();
+
+        fetchedBlocksLength();
+
+        fetchPaginatedBlocks(paginatedId);
     }, []);
+
+    const fetchPaginatedBlocks = paginatedId => async () => {
+        const { data } = await axios.get(`${document.location.origin}/api/blocks/${paginatedId}`);
+        setBlocks(data);
+    }
+
     return (
         <div>
             <div><Link to='/'>Home</Link></div>
             <h3>Blocks</h3>
+            <div>
+                {
+                    [...Array(Math.ceil(blocksLength / 5)).keys()].map(key => {
+                        const paginatedId = key + 1;
+
+                        return (
+                            <span key={key} onClick={fetchPaginatedBlocks(paginatedId)}>
+                                <Button size="sm" variant="danger">
+                                    {paginatedId}
+                                </Button>{' '}
+                            </span>
+                        )
+                    })
+                }
+            </div>
             {
                 blocks.map(block => {
                     return (
@@ -24,7 +51,7 @@ const Blocks = () => {
                 })
             }
         </div>
-    )
+    );
 }
 
 export default Blocks
