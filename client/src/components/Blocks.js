58e3d4851eb27128902859b8a52daa7663ae646c
Badri Narayanan <programmingwithbadri@outlook.com>
2021-01-31 14:10:24 +0530

    fix the missing import

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Block from './Block';
